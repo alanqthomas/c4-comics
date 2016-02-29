6f5638315cfcda4ae97cca88ffbee852ecfafdbc
Alan Thomas <alan.q.thomas@gmail.com>
2016-02-29 03:42:13 -0500

    Add releveant npm packages
    cssmin, jshint, sass, uglify, concat, watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,38 @@
 module.exports = function(grunt) {
 
-	grunt.registerTask('print', function(){
-		console.log("I'm printing to the console");
+	grunt.initConfig({
+		concat: {
+			js: {
+				src: ['war/js/app.js', 'war/js/config.js'],
+				dest: 'war/js/scripts.js'
+			},
+			css: {
+				src: ['war/css/styles.css', 'war/css/noselect.css'],
+				dest: 'war/css/build.css'
+			}
+		},
+		watch: {
+			js: {
+				files: ['war/js/**/*.js'],
+				tasks: ['concat:js']
+			},
+			css: {
+				files: ['war/css/**/*.css'],
+				tasks: ['concat:css']
+			},
+		},
+		cssmin: {
+			build: {
+				files: [
+				],
+				
+			}
+		}
 	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ['concat', 'watch']);
 
 };
---
+++ war/js/scripts.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var app = angular.module('c4', ['ui.router', 'ngCookies' ,'angular-google-gapi']);
+
+// Initialize angular-google-gapi
+app.run(['GAuth', 'GData', 'GApi', '$rootScope', '$window', '$state', 'authService',
+ function(GAuth,   GData,   GApi,   $rootScope,   $window,   $state,   authService){
+				
+		var CLIENT = '432508624556-h5pq1n44nmg5p21r55mubj54cnuptojv.apps.googleusercontent.com';
+		var BASE = '//' + window.location.host + '/_ah/api';
+		
+		GApi.load('userendpoint', 'v1', BASE).then(function(res){
+			console.log("userendpoint: ", res);
+		});
+		
+		GAuth.setClient(CLIENT);
+		GAuth.setScope("https://www.googleapis.com/auth/userinfo.email");
+		
+		
+		// UI-Router state authorization
+		$rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+			var loggedIn = false;
+			authService.checkAuth().then(
+				function(res){
+					loggedIn = res;
+				}			
+			);
+			
+			if(toState.authenticate && !loggedIn){
+				$state.transitionTo("home");
+				event.preventDefault();
+			}
+		});
+		
+		
+}]);
+
+
+"use strict";
+
+(function() {
+	
+var app = angular.module('hci');
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/home");
+	
+	$stateProvider
+		.state('home', {
+			url: "/home",
+			templateUrl: "views/home.html",
+			authenticate: false
+		})
+});
+	
+})();
