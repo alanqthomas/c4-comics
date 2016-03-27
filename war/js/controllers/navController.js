63a3452062a99862def0755c7df5023412305491
MarcRejndrup <marc.rejndrup@stonybrook.edu>
2016-03-27 12:30:50 -0400

    Formatting of dropdown is consistent. logFunc is called by the sign button, but does not work.

--- war/js/controllers/navController.js
+++ war/js/controllers/navController.js
@@ -3,14 +3,15 @@
 (function() {
 angular.module('c4').controller('navCtrl', ['$scope', '$http',
                                     function(	 $scope,   $http){
+	$scope.logMsg = "-";
 	$scope.checkLog = function(){
 		GAuth.checkAuth().then(ifLogin(),ifLogout());
 	}
 	$scope.ifLogin = function() {
-		$scope.logMsg = "Sign Out";
+		$scope.logMsg = "Out";
 	}
 	$scope.ifLogout = function() {
-		$scope.logMsg = "Sign In";
+		$scope.logMsg = "In";
 	}
 	$scope.logFunc = function() {
 		GAuth.checkAuth().then(
