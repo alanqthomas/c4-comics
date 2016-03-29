fd19c801d7ddc8ea514e1499466d8ffbc1b1890a
MarcRejndrup <marc.rejndrup@stonybrook.edu>
2016-03-29 10:44:29 -0400

    Browse options, build structure

--- war/js/controllers/browseController.js
+++ war/js/controllers/browseController.js
@@ -4,8 +4,8 @@
 
 angular.module('c4').controller('browseCtrl', ['$scope', '$http',	'GApi',
                                     function(	$scope,   $http,	 GApi){
-
 		$scope.selTags = $stateParams.list;
+		$scope.displayTags = null;
 		$scope.removeTag = function(tagToRemove){
 			var index = $scope.selTags.indexOf(tagToRemove);
 			if (index > -1){
@@ -15,8 +15,20 @@ angular.module('c4').controller('browseCtrl', ['$scope', '$http',	'GApi',
 		$scope.addTag = function(tagToAdd){
 			$scope.selTags.push(tagToAdd);
 		}
-
+		$scope.getTopTags= function(){
+			//endpoints call to get the top# of tags.
+			//passes in $scope.selTags
+			//result is placed in $scope.displayTags.
+		}
+		$scope.checkResults=function(){
+			//checks if it should load results or tags.
+			//passed in $scope.resultSwitch,  $scope.selTags
+			//calls appropriate function and swaps hidden if necessary.
+		}
+		$scope.getResults= function(){
+			//endpoints call to get the matching comics, sorted by popularity.
+			//passed in $scope.resultSwitch, $scope.selTags
+			//result is placed in $scope.browseResults.
+		}
 }]);
-
-
 })();
