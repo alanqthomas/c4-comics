0f14f13270d26a186dea3544b8881368db4fc9ac
yuzhehu <yuzhe.hu@stonybrook.edu>
2016-03-29 14:45:45 -0400

    doing the js for profile_page

--- war/js/controllers/profileController.js
+++ war/js/controllers/profileController.js
@@ -2,11 +2,59 @@
 	
 (function() {
 
-angular.module('c4').controller('profileCtrl', ['$scope', '$http',
-                                    function(	 $scope,   $http){	
+angular.module('c4').controller('profileCtrl', ['$scope', '$http', 'GApi', 'GAuth',
+                                    function(	 $scope,   $http,  GApi, GAuth){	
 	
 		$scope.msg = "Hello, profile";
+		
+		//modify the DOM to have a getResult method to get the results
+		$scope.getComics=function(){
+			//this is the parameter object
+			var resultReq={
+				"author":$scope.name
+			};
+			//execute using (endpoint, method for endpoint, parameter for method)
+			//then do (if true) $scope.value = resp.items (get the result)
+			//(if false) print error
+			GApi.execute("UserEndpoint", "getComics", resultReq).then(
+				function(resp){
+					$scope.comics=resp.items;
+				},function(resp){
+					console.log("error no result");
+				}
+			);
+		}
+		
+		$scope.getFavorites=function(){
+			var resultReq={
+				"user":$scope.name
+			};
+			GApi.execute("UserEndpoint","getFavorites",resultReg).then(
+				function(resp){
+					$scope.fav=resp.items;
+				},function(resp){
+					console.log("error no favs");
+				}
+			);
+		}
+		
+		$scope.getBio=function(){
+			var resultReq={
+				"user":$scope.name
+			};
+			GApi.execute("UserEndpoints","getBio", resultReq).then(
+				function(resp){
+					$scope.fav=resp.items;
+				},function(resp){
+					console.log("errors no bio");
+				}
+			);
+		}
+		
+		
+		
+		
 }]);
-
-
 })();
+
+
