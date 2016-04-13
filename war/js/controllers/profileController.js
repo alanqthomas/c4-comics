f186518cf7b42ce83e1a766e355750e65e84ea95
yuzhehu <yuzhe.hu@stonybrook.edu>
2016-04-13 17:30:11 -0400

    placeholder done for main page, images now link

--- war/js/controllers/profileController.js
+++ war/js/controllers/profileController.js
@@ -1,84 +1,129 @@
 "use strict";
 (function() {
 //angular.module('c4', ['ngAnimate', 'ui.bootstrap']);
-angular.module('c4').controller('profileCtrl', ['$scope', '$http', 'GApi', 'GAuth', 'GData', '$stateParams',
-                                    function(	 $scope,   $http,  GApi, 	GAuth, 	GData,	 $stateParams){	
+angular.module('c4').controller('profileCtrl', ['$scope', '$http', 'GApi', 'GAuth', 'GData', '$stateParams', "$state",
+                                    function(	 $scope,   $http,  GApi, 	GAuth, 	GData,	 $stateParams,   $state){	
 		//PLACE HOLDERS
 		//this is image url for initial display
-		$scope.series = ['http://media.salon.com/2014/10/archie_comics.jpg'];
-		$scope.favorites = ['http://downloadicons.net/sites/default/files/favorite-icon-47070.png'];
+		$scope.series = [{
+		                  src:'http://media.salon.com/2014/10/archie_comics.jpg',
+		                  id:1}];
+		$scope.favorites = [{
+							src: 'http://downloadicons.net/sites/default/files/favorite-icon-47070.png',
+							id:26}];
+		
 		//this is added for infinite scroll
 		$scope.series_reserves = [
-							"http://cpassets-a.akamaihd.net/images/comic/original/126_lrg-en.gif",
-							"http://nerdist.com/wp-content/uploads/2014/12/BongoSimpsons-1.jpg",
-							"http://www.jinxthemonkey.com/comics/comic_img/comic04-color.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/1.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/2.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/3.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/4.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/5.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/6.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/7.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/8.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/9.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/10.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/11.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/12.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/13.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/14.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/15.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/16.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/17.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/18.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/19.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/20.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/21.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/22.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/23.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/24.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/15/25.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/1.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/2.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/3.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/4.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/5.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/6.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/7.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/8.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/9.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/10.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/11.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/12.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/13.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/14.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/15.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/16.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/17.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/18.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/19.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/20.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/21.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/22.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/23.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/24.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/25.jpg",
-		                   "http://www.readcomics.net/images/manga/the-bunker/16/26.jpg",
-		                   ];
-		$scope.favorites_reserved= [
-		                              'http://www.simchatyisrael.org/wp-content/uploads/2015/08/follow-me.jpg',
-		                              "http://www.readcomics.net/images/manga/deadpool-2016/1/1.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/2.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/3.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/4.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/5.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/6.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/7.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/8.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/9.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/10.jpg",
-		                            "http://www.readcomics.net/images/manga/deadpool-2016/1/11.jpg",
-		                            
-		                           ];
+			{
+				src:"http://cpassets-a.akamaihd.net/images/comic/original/126_lrg-en.gif",
+				id:2
+			},{
+				src: "http://nerdist.com/wp-content/uploads/2014/12/BongoSimpsons-1.jpg",
+				id:3
+			},{
+				src: "http://www.jinxthemonkey.com/comics/comic_img/comic04-color.jpg",
+				id:4
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/1.jpg",
+				id:5
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/2.jpg",
+				id:6
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/3.jpg",
+				id:7
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/4.jpg",
+				id:8
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/5.jpg",
+				id:9
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/6.jpg",
+				id:10
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/7.jpg",
+				id:11
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/8.jpg",
+				id:12
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/9.jpg",
+				id:13
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/10.jpg",
+				id:14
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/11.jpg",
+				id:15
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/12.jpg",
+				id:16
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/13.jpg",
+				id:17
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/14.jpg",
+				id:18
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/15.jpg",
+				id:19
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/16.jpg",
+				id:20
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/17.jpg",
+				id:21
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/18.jpg",
+				id:22
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/19.jpg",
+				id:23
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/20.jpg",
+				id:24
+			},{
+				src:"http://www.readcomics.net/images/manga/the-bunker/15/21.jpg",
+				id:25
+			}];
+		$scope.favorites_reserved= [{
+				src:'http://www.simchatyisrael.org/wp-content/uploads/2015/08/follow-me.jpg',
+				id:27
+          	},{
+          		src:" http://www.readcomics.net/images/manga/deadpool-2016/1/1.jpg",
+          		id:28
+          	},{
+          		src:"http://www.readcomics.net/images/manga/deadpool-2016/1/2.jpg",
+          		id:29
+          	},{
+          		src: "http://www.readcomics.net/images/manga/deadpool-2016/1/3.jpg",
+          		id: 30
+          	},{
+          		src: "http://www.readcomics.net/images/manga/deadpool-2016/1/4.jpg",
+          		id: 31
+          	},{
+          		src:"http://www.readcomics.net/images/manga/deadpool-2016/1/5.jpg",
+          		id: 32
+          	},{
+          		src:"http://www.readcomics.net/images/manga/deadpool-2016/1/6.jpg",
+          		id:33
+          	},{
+          		src:"http://www.readcomics.net/images/manga/deadpool-2016/1/7.jpg",
+          		id:34
+          	},{
+          		src:"http://www.readcomics.net/images/manga/deadpool-2016/1/8.jpg",
+          		id:35
+          	},{
+          		src:"http://www.readcomics.net/images/manga/deadpool-2016/1/9.jpg",
+          		id:36
+          	},{
+          		src:"http://www.readcomics.net/images/manga/deadpool-2016/1/10.jpg",
+          		id:37
+          	},{
+          		src:"http://www.readcomics.net/images/manga/deadpool-2016/1/11.jpg",
+          		id:38
+          	}];
 		//initalize and query for profileEndpoints
 		$scope.profile_id = $stateParams.id;
 		GApi.execute( "c4userendpoint","getC4User", {"id":$scope.profile_id} ).then(
@@ -200,31 +245,53 @@ angular.module('c4').controller('profileCtrl', ['$scope', '$http', 'GApi', 'GAut
 		}
 		$scope.$apply;
 		
+		
+		
+		
+	
+		$scope.series_loadMore = function() {
+			//pushes images to the array. load more
+			if ($scope.series_reserves.length > 0){
+				$scope.series.push($scope.series_reserves.shift());
+			}
+		};
+		$scope.favorites_loadMore = function() {
+			if ($scope.favorites_reserved.length > 0){
+				$scope.favorites.push($scope.favorites_reserved.shift());
+			}
+		}
+		
 		//display tabs base on content
 		$scope.tabs = [];
 		if($scope.series.length>0){
 			$scope.tabs.push({
 				slug: 'series',
 		        title: "Series",
-		        content: $scope.series
+		        content: $scope.series,
+		        load_m: $scope.series_loadMore
 			});
 		}
 		if($scope.favorites.length>0){
 			$scope.tabs.push({
 				slug: 'fav',
 		        title: "Favorites",
-		        content: $scope.favorites
+		        content: $scope.favorites,
+		        load_m: $scope.favorites_loadMore
 			});
 		}
-		$scope.series_loadMore = function() {
-			//pushes images to the array. load more
-			if ($scope.series_reserves.length > 0){
-				$scope.series.push($scope.series_reserves.shift());
+	
+		
+		
+		
+		$scope.go_to_series=function(param_id){
+			if(param_id==null){
+				$state.go('error');
 			}
-			if ($scope.favorites_reserved.length > 0){
-				$scope.favorites.push($scope.favorites_reserved.shift());
+			else{
+				$state.go('series',{"id": param_id});
 			}
-		};
+		}
+		
 }]);
 })();
 
