27e0bb0685fbb71bbd7ba05f045fd2eb11ea550f
Alan Thomas <alan.q.thomas@gmail.com>
2016-04-12 22:37:32 -0400

    Load page endpoint

--- war/js/controllers/comicController.js
+++ war/js/controllers/comicController.js
@@ -2,10 +2,23 @@
 
 (function() {
 
-angular.module('c4').controller('comicCtrl', ['$scope', '$http',
-                                    function(	 $scope,   $http){
+angular.module('c4').controller('comicCtrl', ['$scope', '$http', 'GApi', '$stateParams',
+                                    function(	 $scope,   $http,   GApi,   $stateParams){
 
 		$scope.msg = "Hello, profile";
+
+
+    var id = $stateParams.id;
+
+    GApi.execute("pageendpoint", "getPage", {"id": id}).then(
+      function(res){
+        console.log("Found: ", res);
+      },
+      function(res){
+        console.log("ERROR. Page not found.", res);
+      }
+    );
+
 }]);
 
 
