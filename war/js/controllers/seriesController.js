20f3ee6f0b345a874bdb3d5990fec49b177da124
yuzhehu <yuzhe.hu@stonybrook.edu>
2016-04-20 23:05:42 -0400

    profile subscribing to series now done

--- war/js/controllers/seriesController.js
+++ war/js/controllers/seriesController.js
@@ -36,6 +36,7 @@ angular.module('c4').controller('seriesCtrl', ['$scope', '$http', 'GApi', '$stat
 				$state.go("comic", {"id": id});
 			}
 		}
+		
 		//END init
 		//execute using (endpoint, method for endpoint, parameter for method)
 		GApi.execute("seriesendpoint", "getSeries", {"id":$scope.series_id}).then(
@@ -78,20 +79,49 @@ angular.module('c4').controller('seriesCtrl', ['$scope', '$http', 'GApi', '$stat
 				}
 				$scope.user_id = 0;
 				//check if user is logged in and subbed
-				GAuth.checkAuth().then(
-					function(){
-						$scope.logged_in = true;
-						$scope.user_id = GData.getUser().id;
-						$scope.subbed = $scope.series.subscribers.indexOf($scope.user_id.toString());
-					},
-					function(){
-						$scope.logged_in = false;
-						$scope.user_id = null;
-					}
-				);
+				$scope.update_sub = function() {GAuth.checkAuth().then(
+						function(){
+							$scope.logged_in = true;
+							$scope.user_id = GData.getUser().id;
+							if($scope.series.authorId == $scope.user_id){
+								$scope.is_owner = true;
+							}
+							GApi.execute("seriesendpoint", "getSeries", {"id":$scope.series_id}).then(
+								function(resp){
+									$scope.series = resp;
+									//not updating, thats why
+									if($scope.series.subscribers == null){
+										$scope.subbed = false;
+										console.log("reached sub null check");
+									}
+									else {
+										if($scope.series.subscribers.indexOf($scope.user_id.toString()) >= 0){
+											$scope.subbed = true;
+											console.log("reached index of, true");
+										}
+										else{
+											$scope.subbed = false;
+											console.log("reached index of, false");
+										} 
+									}
+								},
+								function(resp){	
+								}
+							);
+						},
+						function(){
+							$scope.logged_in = false;
+							$scope.user_id = null;
+						}
+					);
+					$scope.is_owner = false;
+				};
+				$scope.update_sub();
+				
 			},function(resp){
 			}
 		);
+		
 		/* attempted ngStyle, they dont work
 		$scope.bg_img="background:#ffffff url('https://www.planwallpaper.com/static/images/6790904-free-background-wallpaper.jpg') no-repeat center center;";
 		$scope.bg_img="{'background-color':'red'}";
@@ -104,12 +134,13 @@ angular.module('c4').controller('seriesCtrl', ['$scope', '$http', 'GApi', '$stat
 				$state.go('profile',{"id": $scope.series.authorId});
 			}
 		}
-		$scope.$apply;
+		
 		//subscribing and unsubscribing
 		$scope.subscribe = function(){
 			GApi.execute("c4userendpoint", "addsubscription", {"userId": $scope.user_id, "seriesId": $scope.series_id}).then(
 				function(resp){
-					
+					$scope.update_sub();
+					//$scope.subbed = true;
 				},
 				function(resp){
 					
@@ -119,7 +150,8 @@ angular.module('c4').controller('seriesCtrl', ['$scope', '$http', 'GApi', '$stat
 		$scope.unsubscribe = function(){
 			GApi.execute("c4userendpoint", "deletesubscription", {"userId": $scope.user_id, "seriesId": $scope.series_id}).then(
 				function(resp){
-					
+					$scope.update_sub();
+					//$scope.subbed = false;
 				},
 				function(resp){
 					
