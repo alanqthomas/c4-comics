9bdd789d1c00cd56de8646dd23b7664d2b8ec402
Alan Thomas <alan.q.thomas@gmail.com>
2016-05-16 05:57:31 -0400

    Browse page styling

--- war/js/controllers/browseController.js
+++ war/js/controllers/browseController.js
@@ -5,7 +5,6 @@
 angular.module('c4').controller('browseCtrl', ['$scope', '$http', 'GApi', '$state', '$stateParams', 'imgService', 'IMG_PREFIXES',
                                     function( $scope, $http, GApi, $state, $stateParams, imgService, IMG_PREFIXES){
 	//init
-	$scope.resultSwitch = 12;
 	$scope.resultsBool = false;
 	$scope.browseResults = [];
 	if($stateParams.tagList != null){
@@ -15,6 +14,8 @@ angular.module('c4').controller('browseCtrl', ['$scope', '$http', 'GApi', '$stat
 	}
 	$scope.displayTags = [];
 	$scope.resultTags = [];
+
+  $scope.defaultPageImg = imgService.getURL(IMG_PREFIXES.PAGE, '123456');
 	//css
 	$scope.tagDisplayStyle = {
 		'background':'rbga(0,0,0,225)',
@@ -31,11 +32,10 @@ angular.module('c4').controller('browseCtrl', ['$scope', '$http', 'GApi', '$stat
 		part.src = imgService.getURL(IMG_PREFIXES.COMIC, part.id);
 		part.type = "comic";
 	}
+
 	$scope.getResults= function(){
-		//console.log($stateParams.tag);
-		//if($scope.selTags.length < 1)
 		if($stateParams.tag == ""){
-			GApi.execute('tagendpoint', 'listTag', {}).then( 
+			GApi.execute('tagendpoint', 'listTag', {}).then(
 				function(resp) {
 					$scope.resultsBool = false;
 					$scope.browseResults = null;
@@ -47,9 +47,7 @@ angular.module('c4').controller('browseCtrl', ['$scope', '$http', 'GApi', '$stat
 					console.log(resp);
 				}
 			);
-		}
-		else{
-			
+		} else{
 			$scope.resultsBool = true;
 			$scope.tagId = $stateParams.tag;
 			//query for comic ids associated with the tag
@@ -60,47 +58,20 @@ angular.module('c4').controller('browseCtrl', ['$scope', '$http', 'GApi', '$stat
 						for(var i = 0; i < resp.comicsWithTag.length; i ++){
 							GApi.execute("comicendpoint", "getComic", {"id":resp.comicsWithTag[i]}).then(
 								function(resp2){
-									//console.log(resp2);
+									console.log(resp2);
 									$scope.browseResults.push(resp2);
 								},
 								function(resp2){
-									
+
 								}
 							);
 						}
 					}
-					
 				},
 				function(resp){
-					
+
 				}
 			);
-			
-			
-			/*
-			var resultReq = {
-				"tags": $scope.selTags,
-				"numResults" : $scope.resultSwitch
-			};
-			GApi.execute('browseendpoint', 'getResults', resultReq).then( 
-				function(resp) {
-					$scope.resultsBool = resp.comics;
-					if($scope.resultsBool){
-						$scope.displayTags = null;
-						$scope.resultTags = null;
-						$scope.browseResults = resp.results;
-						$scope.browseResults.forEach(prepareResult(part));
-					} else {
-						$scope.browseResults = null;
-						$scope.resultTags = resp.results;
-						$scope.displayTags = [];
-						$scope.shiftTags();
-					}
-				}, function(resp) {
-					console.log("Result call failed.");
-					console.log(resp);
-				}
-			);*/
 		}
 	}
 	//tag manipulation functions
@@ -118,7 +89,7 @@ angular.module('c4').controller('browseCtrl', ['$scope', '$http', 'GApi', '$stat
 		$scope.getResults();
 	}*/
 	//navigation
-	$scope.go_to = function(type, id){	
+	$scope.go_to = function(type, id){
 		//console.log("type: " + type + " id: " + id);
 		if(id != null){
 			$state.go('comic',{"id": id});
@@ -128,12 +99,14 @@ angular.module('c4').controller('browseCtrl', ['$scope', '$http', 'GApi', '$stat
 	}
 	//main
 	$scope.getResults();
-	
+
 	$scope.tagClick = function(tag){
 		$state.go('browse', {'tag':tag});
 	}
-	
-	
-	
+
+  $scope.getFirstPageURL = function(id){
+    return imgService.getURL(IMG_PREFIXES.PAGE, id);
+  }
+
 }]);
 })();
